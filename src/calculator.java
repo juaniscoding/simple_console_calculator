91d2671e3cf5a91a92d1f3e60721148339a34366
juaniscoding <149956202+juaniscoding@users.noreply.github.com>
2023-12-04 15:18:09 +0530

    modified by Juan

--- src/calculator.java
+++ src/calculator.java
@@ -7,7 +7,7 @@ import java.lang.*;
 public class calculator {
     public static void main(String[] args) throws InterruptedException{
 
-          System.out.println("This program is made by JUAN SANTHOSH of class VI E  OF SPA");
+          System.out.println("This program is made by JUAN SANTHOSH from class VI E");
 
 
         Scanner sc = new Scanner(System.in);
